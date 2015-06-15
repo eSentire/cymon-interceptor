3f411a99c4c378b70e31ff6b393f49a35988476e
Sam Bowman <sam.bowman@esentire.com>
2015-06-15 08:57:13 -0400

    Basic interface features implemented, need to be fleshed out

--- redirect.js
+++ redirect.js
@@ -1,13 +1,33 @@
-var redirectPage = "redirectPage.html"
+var redirectPage = "redirectPage.html";
+
+function getCymonResponse (url) {
+	var blacklist = ["maps.google.com"];
+	for (var i=0; i < blacklist.length; i++) {
+		if (url.indexOf(blacklist[i]) != -1) {
+            return true;
+		}
+	}
+    return false;
+}
 
 chrome.webRequest.onBeforeRequest.addListener(
 	function(details) {
-		var blocked = ["https://en.wikipedia.org/wiki/Main_Page"];
-		for (var i=0; i < blocked.length; i++) {
-			if (details.url.indexOf(blocked[i]) != -1) {
-				return {redirectUrl: "chrome-extension://" + chrome.i18n.getMessage("@@extension_id") + "/" + redirectPage};
-			}
-		}
+        if (getCymonResponse(details.url)) {
+            if (details.type == "main_frame") {
+                return {redirectUrl: "chrome-extension://" + chrome.i18n.getMessage("@@extension_id") + "/" + redirectPage};
+            } else {
+                chrome.tabs.getSelected(null, function(tab) { //Detailed description: what was blocked, why
+                    chrome.pageAction.show(tab.id);
+                });
+                chrome.notifications.create(NotificationOptions = { //Basic description: "this blocked some stuff"
+                    type: "basic",
+                    title: "Cymon Interceptor",
+                    iconUrl: "icon.png",
+                    message: "A web request on this page was deemed malicious by Cymon and has been cancelled."
+                });
+                return {cancel: true};
+            }
+        }
 	},
 	{urls: ["<all_urls>"]},
 	["blocking"]
