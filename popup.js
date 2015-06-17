3bf1d164ca79bcdaca355ff7e1ee97d5385980b2
Sam Bowman <sam.bowman@esentire.com>
2015-06-17 10:21:39 -0400

    Fixed bug where incorrect button on popup.html was being disabled after whitelisting resources

--- popup.js
+++ popup.js
@@ -1,19 +1,19 @@
 $(window).ready(function() {
     chrome.tabs.query({currentWindow: true, active : true}, function(tabs) {
-
         //Populates each list item
         chrome.extension.sendRequest({method: "retrieveBlockedUrls", tabId: tabs[0].id}, function (response) {
             for (url in response.data) {
                 $("#blocklist").append("<li><span>" + response.data[url] + "</span><button id='allow" + url + "'>Allow</button></li>");
                 $("#allow" + url).click(function() {
+                    var button = this;
                     if (confirm("Are you sure you want to whitelist this domain? This will allow all web requests to this domain, which Cymon believes is malicious.")) {
                         chrome.extension.sendRequest({
                             method: "updateWhitelist",
                             url: response.data[url]
                         }, function (response) {
                             if (response.success) {
-                                $("#allow" + url).attr("disabled", true);
-                                $("#allow" + url).text("Allowed");
+                                button.disabled = true;
+                                button.innerHTML = "Allowed";
                             } else {
                                 $("#response").text("An error has occured: unable to add domain to whitelist.");
                             }
