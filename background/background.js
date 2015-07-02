08e4d5d3e44570bc740f6e2be04bb15fdee2fca6
Sam Bowman <sam.bowman@esentire.com>
2015-07-02 14:26:30 -0400

    Rework in progress

--- background/background.js
+++ background/background.js
@@ -1,50 +1,76 @@
-var imagesUrl = chrome.extension.getURL("../images/");
-var redirectPageUrl = chrome.extension.getURL("../redirect/redirect.html");
-
-function Interceptor () {
-    
-}
-
 /***************************************************************************
-*****************************Class BlockedUrls******************************
+******************************Class Whitelist*******************************
 ***************************************************************************/
+function Whitelist() {
+    this._whitelist = [];
 
-//Class BlockedUrls maintains a dictionary or blocked Urls by tabId
-function BlockedUrls () {
-    this.Urls = {};
+    //Load whitelist from local storage
+    chrome.storage.local.get(function (items) {
+        this._whitelist = items["whitelist"];
+    });
 }
 
-//Returns list of blocked Urls for the given tabId; returns an empty list if no Urls blocked for that tab
-BlockedUrls.prototype.getUrls = function(tabId) {
-    return (this.Urls[tabId] ? this.Urls[tabId] : []);
-}
-
-//Returns count of blocked Urls for given tabId; returns 0 if no Urls blocked for that tab
-//No different than calling blockedUrls.getUrls(tabId).length, but slightly more convenient
-BlockedUrls.prototype.getUrlCount = function(tabId) {
-    return (this.Urls[tabId] ? this.Urls[tabId].length : 0);
-}
-
-//Adds a url to the dictionary of blocked Urls using tabId as a key; initializes a key/value pair for each tabId as needed
-BlockedUrls.prototype.addUrl = function(tabId, url) {
-    if (!this.Urls[tabId]) {
-        this.Urls[tabId] = [];
+Whitelist.prototype.search = function (domain) {
+    if (this._whitelist.indexOf(domain) != -1) {
+        return true;
+    } else {
+        return false;
     }
-    this.Urls[tabId].push(url);
-}
+};
 
-//Removes all entries for a tabId from the dictionary; called whenever a tab is closed.
-BlockedUrls.prototype.dropEntries = function(tabId) {
-    delete this.Urls[tabId];
-}
+Whitelist.prototype.add = function (domain) {
+    if (this._whitelist.indexOf(domain) == -1) {
+        this._whitelist.push(domain);
+        chrome.storage.local.set({"whitelist": this._whitelist});
+    }
+};
+
+Whitelist.prototype.clear = function () {
+    this._whitelist = [];
+    chrome.storage.local.remove("whitelist");
+};
+
+Whitelist.prototype.get = function () {
+    return this._whitelist;
+};
 
 /***************************************************************************
-*****************************Helper Functions*******************************
+******************************Class Blocklist*******************************
 ***************************************************************************/
+
+function Blocklist() {
+    this._blocklist = []
+}
+
+//Returns list of blocked domains
+Blocklist.prototype.get = function() {
+    return this._blocklist;
+};
+
+//Adds a domain to the list of blocked domains
+Blocklist.prototype.add = function(domain) {
+    if (this._blocklist.indexOf(domain) == -1) {
+        this._blocklist.push(domain);
+    }
+};
+
+//Adds a domain to the list of blocked domains
+Blocklist.prototype.clear = function(domain) {
+    this._blocklist = [];
+};
+
+/***************************************************************************
+****************************Helper Functions********************************
+***************************************************************************/
+//Function stub: replace with actual Cymon datafeed in future
+function getCymonInfo() {
+    return $(["*://maps.google.com/*", "*://en.wikipedia.org/*"]).not(whitelist.get()).get();
+};
+
 
 //Updates the count displayed by the browserAction icon in the top bar of Chrome
-function updateBadge(tabId) {
-    var count = blockedUrls.getUrlCount(tabId);
+function updateBadge(blocklist) {
+    var count = blocklist.get().length;
     if (count) {
         chrome.browserAction.setBadgeText({"text": count.toString()});
     } else {
@@ -52,122 +78,61 @@ function updateBadge(tabId) {
     }
 }
 
-function searchWhiteList(url) {
-    for (domain in whitelist){
-        if (url.indexOf(domain) != -1) {
-            return true;
-		}
-    }
-    return false;
-}
-
-//Function stub: replace with actual Cymon datafeed in future
-function getCymonResponse (url) {
-	var blacklist = ["maps.google.com", "en.wikipedia.org"];
-	for (var i=0; i < blacklist.length; i++) {
-		if (url.indexOf(blacklist[i]) != -1) {
-            return true;
-		}
-	}
-    return false;
-}
-
-
-//Callback function to be executed when a web request is intercepted
-function interceptCallback (details) {
-    //Things to do on page load; only for web requests sent directly from browser window
-    if (details.type == "main_frame") {
-        if (details.url != redirectPageUrl) {
-            blockedUrls.dropEntries(details.tabId);
-        }
-        updateBadge(details.tabId);
-        delete tabsNotified[details.tabId];
-    }
-
-    //To be executed if the source is malicioius
-    if (!searchWhiteList(details.url) && getCymonResponse(details.url)) {
-        blockedUrls.addUrl(details.tabId, details.url);
-        if (details.type == "main_frame") { //Call was made from browser; redirect user to safe Cymon page
-            return {redirectUrl: redirectPageUrl};
-        } else { //Call was made within page; block request
-            if (!tabsNotified[details.tabId]) {
-                chrome.notifications.create(NotificationOptions = { //Basic description: "this blocked some stuff"
-                    type: "basic",
-                    title: "Malicious request blocked",
-                    iconUrl: imagesUrl + "cymon-icon.png",
-                    message: "A web request on this page was deemed malicious by Cymon and has been blocked"
-                }, function () {
-                    tabsNotified[details.tabId] = true;
-                });
-            }
-            updateBadge(details.tabId);
-            return {cancel: true};
-        }
-    }
-    return {cancel: false};
-}
-
 /***************************************************************************
-***************************Add Event Listeners******************************
+*********************************"Main"*************************************
 ***************************************************************************/
 
-//Listens for tab change to update badge text
+var whitelist = new Whitelist();
+var blocklists = {}
+
 chrome.tabs.onActivated.addListener(function(activeInfo){
-    updateBadge(activeInfo.tabId);
+    if (!(activeInfo.tabId in blocklists)) {
+        blocklists[activeInfo.tabId] = new Blocklist();
+    }
+    updateBadge(blocklists[activeInfo.tabId]);
 });
 
-//Listens for whenever a tab is closed
+chrome.tabs.onCreated.addListener(function(tab) {
+    blocklists[tab.id] = new Blocklist();
+    updateBadge(blocklists[tab.id]);
+});
+
 chrome.tabs.onRemoved.addListener(function(tabId) {
-    blockedUrls.dropEntries(tabId);
-});
-
-//Listens for requests from popup.js
-chrome.extension.onRequest.addListener(function (request, sender, response) {
-    switch (request.method) {
-        case "retrieveBlockedUrls": //Retrieves the list of all URLs blocked by Cymon
-            chrome.tabs.getCurrent(function () {
-                response({data: blockedUrls.getUrls(request.tabId)});
-            });
-            break;
-        case "addToWhitelist": //Adds an entry to the user's local whitelist
-            var domain = new URL(request.url).hostname;
-            whitelist[domain] = true;
-            chrome.storage.local.set(whitelist);
-            response({success: true});
-            break;
-        case "clearWhitelist": //Clears the whitelist
-            whitelist = {};
-            chrome.storage.local.clear(function(){ //TODO: There has to be some catch for a fail state in here
-                response({success: true});
-            });
-            break;
-        default:
-            response({});
-            break;
+    if (tabId in blocklists) {
+        delete blocklists[tabId];
     }
 });
 
 //Listens for web requests; this is where the magic happens!
 chrome.webRequest.onBeforeRequest.addListener(
-    interceptCallback,
-	{urls: ["<all_urls>"]},
-	["blocking"]
+    function (details) {
+        if (details.type == "main_frame") {
+            if (blocklists[details.tabId]) {
+                blocklists[details.tabId].clear();
+                updateBadge(blocklists[details.tabId]);
+            }
+        }
+
+        var domain = new URL(details.url).hostname
+
+        if (!whitelist.search(domain)) {
+            blocklists[details.tabId].add(domain);
+            if (details.type == "main_frame") { //Call was made from browser; redirect user to safe Cymon page
+                return {redirectUrl: chrome.extension.getURL("/redirect/redirect.html")};
+            } else { //Call was made within page; block request
+                chrome.notifications.create(NotificationOptions = { //Basic description: "this blocked some stuff"
+                    type: "basic",
+                    title: "Malicious request blocked",
+                    iconUrl: "../images/cymon-icon.png",
+                    message: "A web request on this page was deemed malicious by Cymon and has been blocked"
+                });
+                updateBadge(blocklists[details.tabId]);
+                return {cancel: true};
+            }
+        }
+        return {cancel: false};
+    },
+    {urls: getCymonInfo()}, //TODO: If a list of all domains were pulled from cymon and stored locally, it could be used here?
+    ["blocking"]
 );
 
-/***************************************************************************
-*********************************"Main"*************************************
-***************************************************************************/
-
-//Singleton, keeps track of all blocked Urls for a page
-var blockedUrls = new BlockedUrls();
-
-//Keeps track of tabs that have been notified of malicious activity
-var tabsNotified = {}
-
-//List of domains whitelisted by the user
-var whitelist = [];
-
-//Load whitelist from local storage
-chrome.storage.local.get(function(items) {
-    whitelist = items;
-});
--- popup/popup.js
+++ popup/popup.js
@@ -1,49 +1,35 @@
 //Populates each list item
-function loadBlockedUrls(tabId) {
-    chrome.extension.sendRequest({method: "retrieveBlockedUrls", tabId: tabId}, function (response) {
-        if (response.data.length > 0) {
-            $("#description").html("\
-                <h2>The following resources on this page have been blocked:</h2>\
-                <table id='blocklist'></table>\
-            ");
-            for (url in response.data) {
-                $("#blocklist").append("<tr id='blocked" + url + "'><td>" + response.data[url] + "</td><td><button id='whitelist" + url + "'>Whitelist</button></td></tr>");
-                $("#whitelist" + url).click(function() {
-                    var button = this;
-                    if (confirm("Are you sure you want to whitelist " + new URL(response.data[url]).hostname + "? This will allow all web requests to this domain, which Cymon believes is malicious.")) {
-                        chrome.extension.sendRequest({
-                            method: "addToWhitelist",
-                            url: response.data[url]
-                        }, function (response) {
-                            if (response.success) {
-                                button.disabled = true;
-                                button.innerHTML = "Whitelisted";
-                            } else {
-                                $("#response").text("An error has occured: unable to add domain to whitelist.");
-                            }
-                        });
-                    }
-                });
-            }
+function loadBlockedDomains(tabId) {
+    var blockedDomains = chrome.extension.getBackgroundPage().blocklists[tabId].get();
+    if (blockedDomains.length > 0) {
+        $("#description").html("\
+            <h2>The following resources on this page have been blocked:</h2>\
+            <table id='blocklist'></table>\
+        ");
+        for (idomain in blockedDomains) {
+            domain = blockedDomains[idomain];
+            $("#blocklist").append("<tr id='blocked" + idomain + "'><td>" + domain + "</td><td><button id='whitelist" + idomain + "'>Whitelist</button></td></tr>");
+            $("#whitelist" + idomain).click(function() {
+                var button = this;
+                if (confirm("Are you sure you want to whitelist " + domain + "? This will allow all web requests to this domain, which Cymon believes is malicious.")) {
+                    chrome.extension.getBackgroundPage().whitelist.add(domain);
+                    button.disabled = true;
+                    button.innerHTML = "Whitelisted";
+                }
+            });
         }
-    });
+    }
 }
 
 $(window).ready(function() {
     chrome.tabs.query({currentWindow: true, active : true}, function(tabs) {
-        loadBlockedUrls(tabs[0].id);
+        loadBlockedDomains(tabs[0].id);
 
-        //Whitelist button
         $("#clearWhitelist").click(function() {
             if (confirm("Are you sure you want to clear all domains in your whitelist? This action cannot be undone!")) {
-                chrome.extension.sendRequest({method: "clearWhitelist"}, function (response) {
-                    if (response.success) {
-                        $("#clearWhitelist").attr("disabled", true);
-                        $("#clearWhitelist").text("Whitelist Cleared");
-                    } else {
-                        $("#response").text("An error has occured: unable to clear whitelist.");
-                    }
-                });
+                chrome.extension.getBackgroundPage().whitelist.clear();
+                $("#clearWhitelist").attr("disabled", true);
+                $("#clearWhitelist").text("Whitelist Cleared");
             }
         });
     });
